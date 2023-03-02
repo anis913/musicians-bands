aa54107942b2dc4a644ecb0d1a4874f32931e9b8
anis913 <anisislam913@gmail.com>
2023-03-02 23:01:37 +0000

    edited the many to many func

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const {Song} = require('./Song')
 Musician.BelongsTo(Band);
 Band.HasMany(Musician);
 
-Song.belongsToMany(Band, {through: 'song_tags'});
-Band.belongsToMany(Song, {through: 'band_tags'});
+Song.belongsToMany(Band, {through: 'song_band'});
+Band.belongsToMany(Song, {through: 'song_band'});
 
 module.exports = {
     Band,
